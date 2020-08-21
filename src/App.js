b6e41abc145b8fb97f67d33ab765b74cdd83ecdd
mmajje <bluekmky@likelion.org>
2020-08-21 14:35:15 +0900

    route home&enroll

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 
+import { Route } from 'react-router-dom';
+
+import NavBar from './components/Navbar/NavBar'
+import Home from './components/Home/Home';
+import Enroll from './components/Enroll/Enroll'
+
+
 function App() {
   return (
-    <div className="App">
-
-      
-    </div>
+      <div className="App">
+        <div className="ContentBox">
+            <NavBar/>
+            <div className="Content">
+              <Route path="/" exact={true} component={Home} />
+              <Route path="/enroll" component={Enroll} />
+            </div>
+        </div>
+      </div>
+    
   );
 }
 
